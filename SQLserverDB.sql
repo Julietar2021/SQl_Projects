b35a1e7bc1e2259fa984e267877375fb3e5c682d
Juliet Egbule <88993062+Julietar2021@users.noreply.github.com>
2023-09-18 20:44:31 +0200

    Update SQLserverDB.sql

--- SQLserverDB.sql
+++ SQLserverDB.sql
@@ -25,4 +25,33 @@ GRANT SELECT ON cd.work TO demouser;
 -- removea user read only access on cd.work table only
 REVOKE SELECT ON cd.work TO demouser;
 
+USE  AdventureWorks2019
+GO
+
+--create a login for jegbule
+CREATE LOGIN jegbule WITH  PASSWORD='root' MUST_CHANGE, CHECK_EXPIRATION=ON
+
+-- try to login with jegbule by creating a new connection
+
+-- creata user jegbule under adventureworks database for the login to have access
+CREATE USER jegbule FOR LOGIN jegbule
+-- grant jegbule read only access in all tables in adventure db
+
+ALTER ROLE db_datareader ADD MEMBER jegbule
+
+-- remove the role 
+ALTER ROLE db_datareader DROP MEMBER jegbule
+
+DROP LOGIN jegbule
+
+-- kill the user session_id if it cant be drop
+
+SELECT session_id
+from sys.dm_exec_sessions
+WHERE login_name = 'jegbule'
+
+kill 61
+KILL 66
+
+DROP LOGIN jegbule
 
